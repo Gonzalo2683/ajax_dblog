d783e8ba1845d3b12abdc3ce21717cba0867af65
nikhileshgupta <nikhileshgupta@3213755.no-reply.drupal.org>
2016-07-10 14:48:49 -0400

    Issue #2763625 by Nikhilesh Gupta: ajax_dblog ESLint Errors

--- js/ajax.js
+++ js/ajax.js
@@ -12,12 +12,12 @@
    * @param {object} [response.settings]
    * @param {number} [status]
    */
-  Drupal.AjaxCommands.prototype.slideDown = function(ajax, response, status){
+  Drupal.AjaxCommands.prototype.slideDown = function (ajax, response, status) {
     // Get duration if sent, else use default of slow.
     var duration = response.duration ? response.duration : "slow";
     // slide down the selected element(s).
     $(response.selector).slideDown(duration);
-  }
+  };
 
   /**
    * Drupal behavior to remove a log element when it is closed.
@@ -25,30 +25,30 @@
    * @type {{attach: Function}}
    */
   Drupal.behaviors.ajaxDbLog = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       // Find all event close buttons and attach to click event.
-      $(context).find('.dblog-event-close').on('click', function(event){
+      $(context).find('.dblog-event-close').on('click', function (event) {
         // prevent the default action.
         event.preventDefault();
         // Get event-id (wid).
         var wid = $(this).data('event-id');
         // Hide details for the event.
-        $('#dblog-event-details-' + wid).slideUp("slow", function() {
+        $('#dblog-event-details-' + wid).slideUp("slow", function () {
           // Remove the wid details (copied from the core remove command).
           $('#dblog-event-row-' + wid).remove();
         });
       });
     }
-  }
+  };
 
   /**
    * Custom jQuery event to remove container wrapper from ajax inserted tr.
    *  Remove when https://www.drupal.org/node/736066 is resolved.
    */
-  $.fn.unwrapEventRow = function(row_id) {
-   if ($(row_id).parent().is("div")) {
-     $(row_id).unwrap();
-   }
-  }
+  $.fn.unwrapEventRow = function (row_id) {
+    if ($(row_id).parent().is("div")) {
+      $(row_id).unwrap();
+    }
+  };
 
 })(jQuery, this, Drupal, drupalSettings);
